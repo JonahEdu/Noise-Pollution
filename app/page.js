904981ec2a50f74229909775174983ede60d45d9
Jonah Park <jonahpark@Jonahs-MacBook-Pro.local>
2026-03-16 16:45:51 -0700

    added stuff

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import "./page.module.css";
 import CityScene from "@/components/CityScene";
 import BadgeBar from "@/components/BadgeBar";
 import DialogueBox from "@/components/DialogueBox";
 import NoiseMeter from "@/components/NoiseMeter";
 
+
 export default function Home() {
   return (
-    <main className={styles.container}>
+    <main>
       <CityScene />
       <BadgeBar />
       <DialogueBox />
